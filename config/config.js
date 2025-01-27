d1756fb4bc2910bf9d329ae5b2ced53776bf35d4
yassineelmiri <yassireelhima@gmail.com>
2025-01-27 15:12:54 +0100

    Ajout du fichier config.js pour la configuration des paramètres globaux de l'application.

---
+++ config/config.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+module.exports = {
+  mongodb: {
+    uri: process.env.DB_URI
+},
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+};
